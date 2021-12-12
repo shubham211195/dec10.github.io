6ea06245bfb294d46a6f2404883648b0159efb67
shubham211195 <Shubaryan211195@gmail.com>
2021-12-12 20:15:35 +0530

    gg

---
+++ index.js
@@ -0,0 +1,47 @@
+let r = fetch('https://reqres.in/api/user');
+
+let s = r.then(function (response) {
+    return response.json();
+});
+
+s.then(function(a) {
+    // console.log(data);
+    displayData(a.data);
+});
+
+
+function displayData(data) {
+       
+        let table=document.getElementById('table');
+
+        for(let i=0;i<data.length;i++){
+            let obj=data[i];
+        let row= document.createElement('tr');
+       let id= document.createElement('td');
+       let name= document.createElement('td');
+       let year= document.createElement('td');
+       let color= document.createElement('td');
+       let p_value = document.createElement('td');
+
+       id.innerText=obj.id;
+       name.innerText=obj.name;
+       year.innerText=obj.year;
+       color.innerText=obj.color;
+       p_value.innerText=obj.pantone_value;
+        
+
+       row.appendChild(id);
+       row.appendChild(name);
+       row.appendChild(year);
+       row.appendChild(color);
+       row.appendChild(p_value);
+       
+
+
+
+       table.appendChild(row);
+
+       console.log(data)
+    }
+       
+}
